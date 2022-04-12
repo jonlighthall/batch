54adbb0bcfb194f1c8dc535ad840b640e39a0412
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-12 10:36:13 -0500

    initial commit

---
+++ open_progs.bat
@@ -0,0 +1,15 @@
+::@echo off
+
+cd "C:\Users\jlighthall\AppData\Local\Microsoft\OneDrive"
+start OneDrive.exe
+
+cd "C:\Users\jlighthall\AppData\Local\Microsoft\Teams"
+start Update.exe --processStart "Teams.exe"
+
+cd "C:\Program Files (x86)\Microsoft Office\Office16"
+start ONENOTE.EXE
+
+cd "C:\Program Files (x86)\Microsoft Office\Office16"
+start OUTLOOK.EXE
+
+::exit
