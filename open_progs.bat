cbdbb3b376354b7e8fac634c40e9e49bcc7147a9
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-13 14:29:44 -0500

    add running prints

--- open_progs.bat
+++ open_progs.bat
@@ -1,16 +1,16 @@
-::@echo off
+@echo off
 SET STARTDIR="%cd%"
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\OneDrive"
 SET prog=OneDrive.exe
-tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul || (start %prog%)
+tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul && (echo %prog% running) || (start %prog%)
 
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\Teams"
 SET prog=Teams.exe
-tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul || (start Update.exe --processStart "%prog%")
+tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul && (echo %prog% running) || (start Update.exe --processStart "%prog%")
 
 cd "C:\Program Files (x86)\Microsoft Office\Office16"
 FOR %%x IN (ONENOTE.EXE OUTLOOK.EXE) DO (
-    tasklist /nh /fi "imagename eq %%x" | find /i "%%x" > nul ||(start %%x)
+    tasklist /nh /fi "imagename eq %%x" | find /i "%%x" > nul && (echo %%x running) || (start %%x)
     )
 cd %STARTDIR%
 ::exit
