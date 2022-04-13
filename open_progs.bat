a5686dff8bc84e727ef7a804de1c90f941c0ab39
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-13 14:17:02 -0500

    add teams check

--- open_progs.bat
+++ open_progs.bat
@@ -1,11 +1,12 @@
-@echo off
+::@echo off
 SET STARTDIR="%cd%"
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\OneDrive"
 SET prog=OneDrive.exe
 tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul || (start %prog%)
 
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\Teams"
-start Update.exe --processStart "Teams.exe"
+SET prog=Teams.exe
+tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul || (start Update.exe --processStart "%prog%")
 
 cd "C:\Program Files (x86)\Microsoft Office\Office16"
 FOR %%x IN (ONENOTE.EXE OUTLOOK.EXE) DO (
