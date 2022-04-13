693dd70f788ff0d68d2ada31d0e73597da8ffab9
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-13 14:11:54 -0500

    add change back to present directory

--- open_progs.bat
+++ open_progs.bat
@@ -1,4 +1,5 @@
 @echo off
+SET STARTDIR="%cd%"
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\OneDrive"
 SET prog=OneDrive.exe
 tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul || (start %prog%)
@@ -10,4 +11,5 @@ cd "C:\Program Files (x86)\Microsoft Office\Office16"
 FOR %%x IN (ONENOTE.EXE OUTLOOK.EXE) DO (
     tasklist /nh /fi "imagename eq %%x" | find /i "%%x" > nul ||(start %%x)
     )
+cd %STARTDIR%
 ::exit
