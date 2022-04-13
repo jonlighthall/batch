4f66f977826adf671a3821d014072fee16d2b0b5
Jon Lighthall <jon.lighthall@gmail.com>
2022-04-13 14:10:14 -0500

    add loop for office and check for onedrive

--- open_progs.bat
+++ open_progs.bat
@@ -1,15 +1,13 @@
-::@echo off
-
+@echo off
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\OneDrive"
-start OneDrive.exe
+SET prog=OneDrive.exe
+tasklist /nh /fi "imagename eq %prog%" | find /i "%prog%" > nul || (start %prog%)
 
 cd "C:\Users\jlighthall\AppData\Local\Microsoft\Teams"
 start Update.exe --processStart "Teams.exe"
 
 cd "C:\Program Files (x86)\Microsoft Office\Office16"
-start ONENOTE.EXE
-
-cd "C:\Program Files (x86)\Microsoft Office\Office16"
-start OUTLOOK.EXE
-
+FOR %%x IN (ONENOTE.EXE OUTLOOK.EXE) DO (
+    tasklist /nh /fi "imagename eq %%x" | find /i "%%x" > nul ||(start %%x)
+    )
 ::exit
