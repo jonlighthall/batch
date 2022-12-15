00c684b94104f978f005a40a68af3f065e82395c
Jon Lighthall <jon.lighthall@gmail.com>
2022-12-15 12:00:50 -0600

    increase timeout and add sleep

--- open_progs_and_blank.bat
+++ open_progs_and_blank.bat
@@ -28,11 +28,11 @@ SET ppt="%OneDrive%\Desktop\blank.ppsx"
     tasklist /v /fi "imagename eq %prog%" /fi "username eq %USERDOMAIN%\%USERNAME%" | find /i "blank.ppsx" > nul && (echo %ppt% open) || (
         echo opening %ppt%...
         echo ready to open %prog%...
-        timeout /t 60
+        timeout /t 75
         ::start /D %prog_dir% %prog% /S %ppt% && (echo OK) || (echo FAIL)
         start powershell -ExecutionPolicy Unrestricted -File "%USERPROFILE%\Documents\powershell\blank.ps1" && (echo OK) || (echo FAIL)
     )
 
 echo goodbye
-timeout /t 5
+sleep 0.500
 ::exit
