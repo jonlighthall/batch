af8351f076fea15c954f9753c1302f00fc8f1388
Jon Lighthall <jon.lighthall@gmail.com>
2023-02-15 18:17:45 -0600

    add test open progs

---
+++ test_open_progs_and_blank.bat
@@ -0,0 +1,9 @@
+@echo OFF
+FOR %%x IN (POWERPNT.EXE powershell.exe) DO (    
+    tasklist /fi "username eq jlighthall" /fi "imagename eq %%x" && (echo find %%x OK) || (echo find %%x FAIL)
+    taskkill /fi "username eq jlighthall" /fi "imagename eq %%x" && (echo kill %%x OK) || (echo kill %%x FAIL)        
+    )
+
+start open_progs_and_blank.bat
+echo done
+echo goodbye
