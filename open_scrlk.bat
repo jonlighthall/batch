3c43adfb30a9d752000c5ad6d51ebd016485ce0c
Jon Lighthall <jon.lighthall@gmail.com>
2025-11-25 10:31:08 -0600

    remove unused variable

--- open_scrlk.bat
+++ open_scrlk.bat
@@ -1,5 +1,4 @@
 @echo off
-SET STARTDIR="%cd%"
 echo|set /p="opening... "
 start powershell -ExecutionPolicy Unrestricted -File "%USERPROFILE%\Documents\powershell\scrlk.ps1" && (echo OK) || (echo FAIL)
 
